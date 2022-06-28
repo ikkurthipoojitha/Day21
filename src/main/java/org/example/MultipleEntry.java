cc3bb5bda06bcf87d67abf43d53a8e0a3ad0a7f4
ikkurthipoojitha <ipoojitha1997@gmail.com>
2022-06-28 09:15:54 +0530

    updated

--- src/main/java/org/example/MultipleEntry.java
+++ src/main/java/org/example/MultipleEntry.java
@@ -4,20 +4,11 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
 public class MultipleEntry {
-
-    public boolean mailID(String test) {
-        String mail = "^[a-z]{3}[_.+][a-z0-9]{3}[@][a-z]{2}[.][c]{1}[o]{1}[.][a-z]{2,5}$";
-        Pattern regex = Pattern.compile(mail);
-        Matcher mailIDMatcher = regex.matcher(test);
-        if (mailIDMatcher.matches()) {
-            System.out.println(test + " is valid email.");
-            return true;
-        }
-        else {
-            System.out.println(test + "  is invalid email");
-            return false;
-        }
-
+    public static boolean validateMail(String regex, String test) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(test);
+        return matcher.matches();
     }
 }
